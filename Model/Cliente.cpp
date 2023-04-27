b0014096b8bfeb53d22acb6256f9f17874232916
Martina-mey <matumeyer0109@gmail.com>
2023-04-26 22:03:34 -0300

    cambios en la clase cliente

--- Model/Cliente.cpp
+++ Model/Cliente.cpp
@@ -14,15 +14,14 @@ void Cliente::Abonar(Ticket miTicket) {
 }
 
 //constructor
-Cliente::Cliente(string Nombre, string Apellido, string DNI, string NroAfiliado, unsigned int CodigoSeguridad, string Receta, unsigned int NroTicket, bool SoloKiosco) :NroAfiliado(NroAfiliado) {
+Cliente::Cliente(string Nombre, string Apellido, string DNI, unsigned int CodigoSeguridad, unsigned int NroTicket) {
 	this->nombre = Nombre;
 	this->apellido = Apellido;
 	this->dni = DNI;
 	this->codigoSeguridad = CodigoSeguridad;
-	this->receta = Receta;
 	this->mail = Mail;
 	this->nroTicket = NroTicket;
-	this->soloKiosco = SoloKiosco;
+
 }
 
 //destructor
@@ -40,17 +39,11 @@ string Cliente::get_DNI() {
 	return this->DNI;
 }
 
-const string Cliente::get_NroAfiliado() {
-	return this->NroAfiliado;
-}
 
 unsigned int Cliente::get_CodigoSeguridad() {
 	return this->CodigoSeguridad;
 }
 
-string Cliente::get_Receta() {
-	return this->Receta;
-}
 
 string Cliente::get_NroTicket() {
 	return this->NroTicket;
--- Model/Cliente.h
+++ Model/Cliente.h
@@ -4,7 +4,7 @@
 
 class Cliente {
 public: 
-Cliente(string Nombre, string Apellido, string DNI, string NroAfiliado, unsigned int CodigoSeguridad, string Receta, unsigned int NroTicket, bool SoloKiosco) :NroAfiliado(NroAfiliado);
+Cliente(string Nombre, string Apellido, string DNI, unsigned int CodigoSeguridad, unsigned int NroTicket);
 ~Cliente();
 unsigned int SacarTicket(AsistAutomatico miAsistAutomatico);
 void EntregarSolicitado();
@@ -12,21 +12,17 @@ void Abonar(Ticket miTicket);
 string get_Mail();
 unsigned int set_CodigoSeguridad(unsigned int CodigoSeguridad);   
 string get_DNI();   
-const string get_NroAfiliado();   
 unsigned int get_CodigoSeguridad();
-string get_Receta();
 unsigned int get_NroTicket();
 
 private: 
     string Nombre;
     string Apellido;
     string DNI;
-    const string NroAfiliado;
     unsigned int CodigoSeguridad;
-    string Receta;
     string Mail;
     unsigned int NroTicket;
-    void SoloKiosco; bool;
+
 };
 
 #endif //_CLIENTE_H
